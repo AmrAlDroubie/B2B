bb6eb5e7494302013e512e9bb532eb19eb59cc60
amraldroubie <amraldroubie@hotmail.com>
2022-05-20 22:57:53 +0300

    new section

--- js/script.js
+++ js/script.js
@@ -70,3 +70,71 @@ const our_program = new Swiper(".our-program-swiper", {
         disableOnInteraction: false,
     },
 });
+
+const leading_ecommerce = new Swiper(".leading-ecommerce-swiper", {
+    speed: 300,
+    loop: true,
+    spaceBetween: 15,
+    autoplay: {
+        delay: 400,
+        disableOnInteraction: false,
+    },
+    slidesPerView: 2,
+    breakpoints: {
+        992: {
+            slidesPerView: 6,
+        },
+    },
+});
+
+const our_systems_swiper = new Swiper(".our-systems-swiper", {
+    speed: 300,
+    spaceBetween: 15,
+    pagination: {
+        el: ".our-systems-pag",
+        type: "bullets",
+        clickable: true,
+    },
+    mousewheel: true,
+});
+
+// Our Systems Menu
+const systems_menu_items = document.querySelectorAll(".our-systems-menu ul li");
+const systems_swiper_wrapper = document.querySelector(
+    ".our-systems .swiper-wrapper"
+);
+
+// activate menu item
+systems_menu_items.forEach((ele) => {
+    ele.onclick = function() {
+        our_systems_swiper.slideTo(this.getAttribute("data-slide-index"));
+
+        let other_elements = [...systems_menu_items].filter(function(e) {
+            return e !== this;
+        });
+
+        other_elements.forEach((other_ele) => {
+            other_ele.classList.remove("active");
+        });
+        this.classList.add("active");
+    };
+});
+
+our_systems_swiper.on("slideChange", function() {
+    // console.log(this.activeIndex);
+    const active_now_slide = document.querySelector(
+        `[data-slide-index='${this.activeIndex}']`
+    );
+    let other_elements = [...systems_menu_items].filter(function(e) {
+        return e !== active_now_slide;
+    });
+
+    other_elements.forEach((other_ele) => {
+        other_ele.classList.remove("active");
+    });
+    active_now_slide.classList.add("active");
+
+    if (our_systems_swiper.slides.length == this.activeIndex + 1) {
+        our_systems_swiper.mousewheel.disable();
+    }
+});
